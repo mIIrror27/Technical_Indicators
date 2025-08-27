06a67fdeaa3133ca3842531d704fcfd8e30a34ca
mIIrror27 <quaratile@proton.me>
2025-08-27 14:35:03 +0530

    Update bullish_divergence.py
    Updating lookback period

--- RSI/bullish_divergence.py
+++ RSI/bullish_divergence.py
@@ -4,7 +4,7 @@ import numpy as np
 import datetime as dt
 import talib
 
-api_key = ""
+api_key = "muenox7bwqjnv51m"
 access_token = ""
 with open("access_token.txt") as f:
     access_token = f.read().strip()
@@ -14,7 +14,7 @@ kite.set_access_token(access_token)
 exchange = "NSE"
 
 token = 1703937
-start_date = dt.datetime(2021, 1, 15)
+start_date = dt.datetime(2025, 1, 15)
 end_date = dt.datetime(2025, 8, 22)
 
 data = kite.historical_data(
@@ -28,21 +28,21 @@ df = pd.DataFrame(data)
 
 df["rsi"] = talib.RSI(df["close"], timeperiod= 14)
 
-# Calculating local minima
-df["SwingP_Low"] = (df["close"] <  df["close"].shift(1)) & (df["close"] < df["close"].shift(-1))
-df["SwingR_Low"] = (df["rsi"] <  df["rsi"].shift(1)) & (df["rsi"] < df["rsi"].shift(-1))
+lookback1 = 5
+df["SwingP_Low"] = df["close"] == df["close"].rolling(lookback1).min()
+df["SwingR_Low"] = df["rsi"] == df["rsi"].rolling(lookback1).min()
 
-# Creating Swing condition dataframe
+
 swing = df[(df["SwingP_Low"]) & (df["SwingR_Low"])].copy()
 
 
 signal = []
-lookback = 30
+lookback2 = 30
 
 for i in range(1, len(swing)):
     curr = swing.iloc[i]
 
-    for j in range(max(0, i - lookback), i):
+    for j in range(max(0, i - lookback2), i):
         prev = swing.iloc[j]
 
         if (curr["close"] < prev["close"]) and (curr["rsi"] > prev["rsi"]):
